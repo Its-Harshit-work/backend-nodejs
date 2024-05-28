e874d71f1bb4e304c52128feb98a7ded257b9433
Harshit Rai <workwithharshitrai@gmail.com>
2024-05-29 00:02:05 +0530

    minor updates

--- express/newindex.js
+++ express/newindex.js
@@ -1,14 +1,25 @@
 // const http = require("http")
-// const fs=require("fs");
-// const url = require("url");
+const fs=require("fs");
+const url = require("url");
+const path = require("path");
 // const { send } = require("process");
 
 //app is a handler function here
 const express = require("express");
 const app=express();
+const port=8000;
+
+app.use((req, res, next) => {
+    const log = `${new Date().toISOString()} - ${req.method} ${req.url}\n`;
+    fs.appendFile(path.join(__dirname, "log.txt"), log, (err) => {
+      if (err) {
+        console.error("Error writing to log file", err);
+      }
+    });
+    next();
+  });
 
 app.get("/", (req, res) =>{
-    const log = `${Date.now()}: ${req.method} ${req.url} New Req Recieved\n`;
     return res.send(`Hello ${req.query.name}`);
 });
 
@@ -16,7 +27,7 @@ app.get("/about", (req, res)=>{
     return res.send("Hello in the about page")
 })
 
-app.listen(8000, ()=> console.log("Server Started"));
+app.listen(port, ()=> console.log(`Server Started on port ${port}`));
 // const myServer = http.createServer((app));
 // myServer.listen(8000, () => console.log("Server Started"));
 
