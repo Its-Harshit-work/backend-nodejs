fc9598f610b9ca631290cbe64087d5b1f58c97e3
Harshit Rai <workwithharshitrai@gmail.com>
2024-05-30 23:58:21 +0530

    changes in index.js

---
+++ express/new1index.js
@@ -0,0 +1,34 @@
+// const myServer = http.createServer((app));
+// myServer.listen(8000, () => console.log("Server Started"));
+
+
+const myServer = http.createServer((req, res) => {
+    if(req.url=="/favicon.ico") return res.end();
+    // const log = `${Date.now()}: New Req Recieved\n`;
+    const log = `${Date.now()}: ${req.method} ${req.url} New Req Recieved\n`;
+    const myUrl = url.parse(req.url, true);
+    // console.log(myUrl);
+    fs.appendFile("log.txt", log , (err, data) =>{
+        switch(myUrl.pathname){
+            case("/"):
+                if(req.method==="GET") res.end("Hello in the home page");
+                break;
+            case("/about"):
+                const username=myUrl.query.myname;
+                res.end(`Hello, ${username} This is Harshit and you will know more about me with others`);;
+                break;
+            case("/signup"):
+                if(req.method==="GET") res.end("This is a signup form");
+                else if (req.method==="POST"){
+                    //DB Query
+                    res.end("Success");
+                }
+                break;
+            default:
+                res.end("404 Not Found");
+        }
+        // res.end("Hello from server Again");
+    })
+    // console.log(req.headers);
+    
+});
